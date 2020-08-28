d32b34970e6a78287e956aebdf0391adbe133db6
Peet Klecha <p.a.klecha@gmail.com>
2020-08-28 14:09:43 -0400

    babel-loader preset

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@ module.exports = {
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
 				loader: "babel-loader",
+				options: {
+					presets: ["@babel/preset-react"],
+				},
 			},
 		],
 	},
